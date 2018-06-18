d4cda23657e370299b44ad67b5e2dd6749e08ce7
PirkeDom <dominic.pirker@infineon.com>
2018-06-18 12:02:11 +0200

    add marker, add automatic wifi scan (1000times)

--- Task2_Localization/app/src/main/java/com/mobilecomputing/dominic/task2_localization/MainActivity.java
+++ Task2_Localization/app/src/main/java/com/mobilecomputing/dominic/task2_localization/MainActivity.java
@@ -12,9 +12,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -28,18 +32,28 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Stream;
 
 public class MainActivity extends Activity {
 
+    private int[] markerPosX = new int[]{30,150,260,370,460,530,370,630,740,840};
+    private int[] markerPosY = new int[]{920,920,820,920,820,820,820,820,820,820};
+
     private TextView textCellNr;
     private TextView textRecording;
     private TextView textCellProbabilities;
+    private TextView textMarker;
 
     private WifiManager wifi;
     private int currentCellNr = -1;
@@ -59,6 +73,54 @@ public class MainActivity extends Activity {
     private int CELL_NR = 3; //TODO: IMPORTANT: set right cell number
     private int RSS_NR = 256;
 
+    private void refreshWifiData() {
+        try {
+            wifi.startScan();
+            List<ScanResult> results = wifi.getScanResults();
+
+            if (currentCellNr == -1)
+                return;
+
+            for (int n = 0; n < results.size(); n++) {
+                ScanResult resultAP = results.get(n);
+                // SSID contains name of AP and level contains RSSI
+                System.out.printf("Wifi", "SSID = " + resultAP.SSID + "; BSSID = " + resultAP.BSSID.replace(':', '_') + "; RSSI =  " + resultAP.level);
+
+                if (accessPoints.containsKey(resultAP.BSSID.replace(':', '_')) == false)
+                    accessPoints.put(resultAP.BSSID.replace(':', '_'), new HashMap<Integer, List<Double>>());
+
+                if (accessPoints.get(resultAP.BSSID.replace(':', '_')).containsKey(currentCellNr) == false)
+                    accessPoints.get(resultAP.BSSID.replace(':', '_')).put(currentCellNr, new ArrayList<Double>());
+
+                accessPoints.get(resultAP.BSSID.replace(':', '_')).get(currentCellNr).add(new Double(resultAP.level));
+
+            }
+
+        } catch (Exception ex) {
+
+        }
+    }
+
+    private static HashMap sortByValues(HashMap map) {
+        List list = new LinkedList(map.entrySet());
+        // Defined Custom Comparator here
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o2)).getValue())
+                        .compareTo(((Map.Entry) (o1)).getValue());
+            }
+        });
+
+        // Here I am copying the sorted list in HashMap
+        // using LinkedHashMap to preserve the insertion order
+        HashMap sortedHashMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedHashMap;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +129,14 @@ public class MainActivity extends Activity {
         textCellNr = (TextView) findViewById(R.id.text_cellnr);
         textRecording = (TextView) findViewById(R.id.label_recording);
         textCellProbabilities = (TextView) findViewById(R.id.label_cellProbability);
+
+        textMarker = (TextView) findViewById(R.id.label_marker);
+
+        textMarker.setX(markerPosX[0]);
+        textMarker.setY(markerPosY[0]);
+
+       // textMarker.layout(10,500,0,0);
+
 
         // Initialize WifiManager (contains API for managing all aspects of Wi-Fi connectivity)
         wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -78,6 +148,7 @@ public class MainActivity extends Activity {
         registerReceiver (new WifiReceiver(), new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         // begin scan
         wifi.startScan();
+
 
         final Button buttoncreateFile = (Button)findViewById(R.id.button_createFile);
         buttoncreateFile.setOnClickListener(new View.OnClickListener(){
@@ -169,13 +240,38 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
+                if (doLocalization == false) {
+                    return;
+                }
+                currentResults = new HashMap<>();
+                List<ScanResult> results = wifi.getScanResults();
+                for (int i = 0; i < results.size(); i++) {
+                    currentResults.put(results.get(i).BSSID.replace(':', '_'), (int) (128.0 + results.get(i).level));
+
+                }
 
                 if(currentResults == null)
                     return;
 
+                //TODO: maybe find better way to sort and trim hashmap
+                //only use 4 strongest values
+                HashMap<String, Integer> map = sortByValues(currentResults);
+                HashMap trimmedHashMap = new LinkedHashMap();
+                Iterator it = map.entrySet().iterator();
+                int count = 0;
+                while (it.hasNext() && count < 4) {
+                    Map.Entry pair = (Map.Entry)it.next();
+                    System.out.println(pair.getKey() + " = " + pair.getValue());
+                    trimmedHashMap.put(pair.getKey(), pair.getValue());
+                    it.remove(); // avoids a ConcurrentModificationException
+                    count++;
+                }
+                currentResults = trimmedHashMap;
+
                 HashMap<String, HashMap<Integer, double[]>> accessPointsPosterior = new HashMap<>();
 
 
+                //loop over access points
                 for (HashMap.Entry<String, HashMap<Integer, double[]>> entryAP : accessPointsNormalizedHistogram.entrySet()) {
                     String keyAP = entryAP.getKey();
                     if(currentResults.containsKey(keyAP) == false)
@@ -183,21 +279,39 @@ public class MainActivity extends Activity {
 
                     HashMap<Integer, double[]> valueAP = entryAP.getValue();
 
+                    //loop over cells (rows of access points)
                     for (HashMap.Entry<Integer, double[]> entryCell : valueAP.entrySet()) {
                         Integer keyCell = entryCell.getKey();
                         double[] valueCell = entryCell.getValue();
                         for(int i = 0; i < valueCell.length; i++)
                         {
+                            double newCellProbability = cellProbabilities.get(keyCell) * valueCell[currentResults.get(keyAP)];
+
+                             // Any cell may contain the agent with a non-zero probability
+                            double chance = 1/(cellProbabilities.size() * 100.0);
+                            if (newCellProbability < chance)
+                                newCellProbability = chance;
+
+                            // normalize
                             double sumColumn = 0.0;
                             for (HashMap.Entry<Integer, double[]> tmp : valueAP.entrySet())
                                 sumColumn += tmp.getValue()[currentResults.get(keyAP)];
+                            if(sumColumn > 0)
+                                newCellProbability /= sumColumn;
 
-                            double newCellProbability = cellProbabilities.get(keyCell) * valueCell[currentResults.get(keyAP)] / sumColumn; //TODO: find probability p(rss_j)
                             cellProbabilities.put(keyCell, newCellProbability);
                         }
                     }
                 }
                 currentResults = null;
+
+                //TODO: mark cell with highest probability
+                HashMap<String, Integer> tmp = sortByValues(cellProbabilities);
+                int cell_nr_to_mark = (int)tmp.keySet().toArray()[0];
+
+
+                textMarker.setX(markerPosX[cell_nr_to_mark - 1]);
+                textMarker.setY(markerPosY[cell_nr_to_mark - 1]);
 
                 textCellProbabilities.setText(getResources().getString(R.string.label_cellProbability, cellProbabilities.get(1), cellProbabilities.get(2), cellProbabilities.get(3),
                         cellProbabilities.get(4), cellProbabilities.get(5), cellProbabilities.get(6), cellProbabilities.get(7), cellProbabilities.get(8), cellProbabilities.get(9),
@@ -276,6 +390,9 @@ public class MainActivity extends Activity {
                 try {
                     currentCellNr = Integer.parseInt(textCellNr.getText().toString());
                     textRecording.setText("RECORDING");
+
+                    for(int a = 0; a < 1000; a++)
+                        refreshWifiData();
                 }
                 catch (Exception ex){
                     currentCellNr = -1;
@@ -298,7 +415,7 @@ public class MainActivity extends Activity {
     class WifiReceiver extends BroadcastReceiver {
         // An access point scan has completed and results are sent here
         public void onReceive(Context c, Intent intent) {
-
+/*
             if (doLocalization == true) {
                 currentResults = new HashMap<>();
                 List<ScanResult> results = wifi.getScanResults();
@@ -308,10 +425,12 @@ public class MainActivity extends Activity {
                 }
                 return;
             }
+*/
 
+
+            /*
             if (currentCellNr == -1)
                 return;
-
             List<ScanResult> results = wifi.getScanResults();
             //while(results.size() == 0)
                 //results = wifi.getScanResults();
@@ -332,7 +451,7 @@ public class MainActivity extends Activity {
 
                 }
             } catch (Exception e) {
-            }
+            }*/
         }
     } // End of class WifiReceiver
 }
